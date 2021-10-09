392cf86268d83a740a27adb503cb9cdf23b85383
Cristian Echeverria <cristian.echeverri4@gmail.com>
2021-10-09 11:34:58 +0200

    Updates Chats styles

--- src/screens/Chats/styles.js
+++ src/screens/Chats/styles.js
@@ -3,6 +3,7 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 10,
   },
   header: {
     flex: 2,
@@ -15,5 +16,8 @@ export const styles = StyleSheet.create({
     borderTopRightRadius: 30,
     paddingHorizontal: 20,
     paddingVertical: 30,
+  },
+  ml10: {
+    marginLeft: 10,
   },
 });
